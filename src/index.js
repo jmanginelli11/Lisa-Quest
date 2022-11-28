5c9962fdfbc44fdbe29c31d1064d9e8158ad0f18
Naomi Brender <NaomiBBrender@gmail.com>
2022-11-28 16:16:51 -0500

    play button image red and white

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import * as Phaser from 'phaser';
-import GameScene from './GameScene.js';
+import * as Phaser from "phaser";
+import GameScene from "./GameScene.js";
 
 const config = {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
+  parent: "phaser-example",
   width: 800,
   height: 600,
   scene: GameScene,
+
+  render: {
+    pixelArt: true,
+  },
 };
 
 const game = new Phaser.Game(config);
