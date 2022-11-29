3618e431aba4aa35e1abaa917ab15115bbf930ae
Jackie Manginelli <jmanginelli11@gmail.com>
2022-11-29 12:26:25 -0500

    switch scenes first go

--- src/GameScene.js
+++ src/GameScene.js
@@ -1,31 +1,16 @@
 import { Scene } from "phaser";
 
 class GameScene extends Scene {
+  constructor() {
+    super({ key: "GameScene" });
+  }
+
   preload() {
-    this.load.image("logo", "/assets/menu/logo.png");
     this.load.image("sky", "/assets/menu/sky.png");
-    this.load.image("play-red", "/assets/menu/play_red.png");
-    this.load.image("play-white", "/assets/menu/play_white.png");
   }
 
   create() {
     this.add.image(400, 300, "sky");
-
-    let playButton = this.add.image(400, 300, "play-white").setScale(5);
-
-    playButton.setInteractive();
-
-    playButton.on("pointerover", () => {
-      playButton = this.add.image(400, 300, "play-red").setScale(5);
-    });
-
-    playButton.on("pointerout", () => {
-      playButton = this.add.image(400, 300, "play-white").setScale(5);
-    });
-
-    playButton.on("pointerup", () => {
-      console.log("lesssss gooooooooooo");
-    });
   }
 }
 
