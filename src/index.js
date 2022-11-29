65e4da6c91e5b5ca9a72963309defc3e5bf349a7
Sheyla De los Santos <delossantosshey@gmail.com>
2022-11-29 17:56:22 -0500

    Added the form scene to the main scene to be able to see it now

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import MainMenu from "./scenes/MainMenu.js";
 import GameScene from "./scenes/GameScene";
 import GameScore from "./scenes/GameScore";
 import HighScores from "./scenes/HighScores";
-import Credits from "./scenes/Credits";''
+import Credits from "./scenes/Credits";
+import Form from "./scenes/Form.js";
 // import { MainMenu, GameScene, GameScore, HighScores, Credits } from "./scenes";
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
+  dom: { createContainer: true },
   width: 800,
   height: 600,
-  scene: [MainMenu, GameScene, GameScore, HighScores, Credits],
+  scene: [MainMenu, GameScene, GameScore, HighScores, Credits, Form],
 
   render: {
     pixelArt: true,
