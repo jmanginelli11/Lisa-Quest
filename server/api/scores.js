1e449def958660a4b3b574b3cc74b69770ca5bb2
Sheyla De los Santos <delossantosshey@gmail.com>
2022-11-30 11:18:12 -0500

    Back end routes for getting and adding scores

---
+++ server/api/scores.js
@@ -0,0 +1,22 @@
+const scoresRouter = require("express").Router();
+const { Score } = require("../db");
+
+scoresRouter.get("/", async (req, res, next) => {
+  try {
+    const scores = await Score.findAll();
+    res.status(200).send(scores);
+  } catch (error) {
+    next(error);
+  }
+});
+
+scoresRouter.post("/", async (req, res, next) => {
+  try {
+    const score = await Score.create(req.body);
+    res.status(201).send(score);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = scoresRouter;
