01cb46174cdf3d917e7ae0775c1975a5a3f5fea1
Sheyla De los Santos <delossantosshey@gmail.com>
2022-11-30 18:07:03 -0500

    a few minor changes: config(parent), added a parent in the index.html, and edited the index api

--- server/index.js
+++ server/index.js
@@ -13,17 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 //static file-serving middleware
 app.use(express.static(path.join(__dirname, '..', '/public')));
 
-// auth and api routes
+//api routes
 app.use('/api', require('./api'));
 
 // sends index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '/./public/index.html'));
+  res.sendFile(path.join(__dirname, '..', '/public/index.html'));
 });
-
-// app.use('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', '/./public/index.html'));
-// });
 
 // error handling endware
 app.use((err, req, res, next) => {
