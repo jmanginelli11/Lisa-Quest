bafe40c3486e4bda152e10dcb086808bf1ec25fa
Naomi Brender <NaomiBBrender@gmail.com>
2022-12-01 20:15:35 -0500

    intro story

--- src/scenes/Intro.js
+++ src/scenes/Intro.js
@@ -3,46 +3,32 @@ import { Scene } from 'phaser';
 class Intro extends Scene {
   constructor() {
     super({ key: 'Intro' });
-    // const story = "here is our intro story oOoOoOoOoOoOoOo"
   }
 
   create() {
     const x = innerWidth / 2;
     const y = innerHeight / 2;
 
-    // space = this.add.tileSprite(x, y, 800, 600, 'stars');
-
     this.add.image(x, y, 'stars');
 
-    this.text = this.add.text(x, y, 'here is our intro story oOoOoOoOoOoOoOo');
+    this.story = this.add.text(x - 400, y - 200, '');
 
-    this.tweens.add({
-      targets: this.text,
-      scroll: true,
-      loop: -1,
-    });
-    // this.wordsOnScreen = this.add.text(x, y, '');
-
-    // this.typewriteText(
-    //   "I'm the Intro Scene! I'm the Intro Scene! I'm the Intro Scene! I'm the Intro Scene! I'm the Intro Scene!"
-    // );
+    this.typewriteText(
+      "                Earth is dying. \n                \nEvil capitalist have selfishly mined our world to the edge of destruction.\n                \nA small, peacefull contingent of humans have put their hopes in you, Lisa.\n                \nYou're mission is the find a habitable new planet where humans can live \npeacefully, in balace with nature.\n                \nThis planet looks promising!\n                \nBut wait, what's that?\n                \nOh no! The capitalists are here to destroy this planet too!\n                \nWe can't have that! "
+    );
   }
-
-  // typewriteText(text) {
-  //   const length = text.length;
-  //   let i = 0;
-  //   this.time.addEvent({
-  //     callback: () => {
-  //       this.wordsOnScreen.text += text[i];
-  //       i++;
-  //     },
-  //     repeat: length - 1,
-  //     delay: 2000,
-  //   });
-  // }
-  // update() {
-  //   space.tilePosition.y += 2;
-  // }
+  typewriteText(text) {
+    const length = text.length;
+    let i = 0;
+    this.time.addEvent({
+      callback: () => {
+        this.story.text += text[i];
+        i++;
+      },
+      repeat: length - 1,
+      delay: 50,
+    });
+  }
 }
 
 export default Intro;
