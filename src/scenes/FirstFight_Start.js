6afa9ef4a89feb95b43d70ff40a5d984dfa90a75
Sheyla De los Santos <delossantosshey@gmail.com>
2022-12-05 16:51:17 -0500

    changes to the the other two first fight sublevels

--- src/scenes/FirstFight_Start.js
+++ src/scenes/FirstFight_Start.js
@@ -13,12 +13,14 @@ class FirstFight_Start extends Scene {
     // Background - First Scene
 
     this.sun = this.add.image(0, 0, 'sun').setOrigin(0, 0);
+    this.sun.displayWidth = this.sys.canvas.width;
+    this.sun.displayHeight = this.sys.canvas.height;
     this.map = this.make.tilemap({ key: 'tilemap_FF' });
 
     this.groundTileset = this.map.addTilesetImage('ground_tileset', 'tiles');
 
     this.rocksAndPlantsTileset = this.map.addTilesetImage(
-      'rock_and_pants_tileset',
+      'rock_and_plants_tileset',
       'vegetation2'
     );
 
@@ -37,8 +39,14 @@ class FirstFight_Start extends Scene {
     );
 
     this.player = new Lisa(this, 0, 0).setOrigin(0, 0);
+    this.groundAndPlatforms.displayWidth = this.sys.canvas.width;
+    this.groundAndPlatforms.displayHeight = this.sys.canvas.height;
+    this.rocksAndPlants.displayWidth = this.sys.canvas.width;
+    this.rocksAndPlants.displayHeight = this.sys.canvas.height;
     this.physics.add.collider(this.player, this.groundAndPlatforms);
+    // this.groundAndPlatforms.setCollisionBetween(27, 79);
     this.groundAndPlatforms.setCollisionBetween(142, 170);
+    this.groundAndPlatforms.setCollisionBetween(743, 746);
   }
 
   update() {
