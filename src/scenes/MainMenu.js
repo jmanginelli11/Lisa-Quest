4e3cc3dc810471faa54c56a419d7a1ab9052ec26
Sheyla De los Santos <delossantosshey@gmail.com>
2022-12-06 17:49:56 -0500

    added new png for play and high score buttons

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -8,22 +8,29 @@ class MainMenu extends Scene {
   create() {
     const x = innerWidth / 2;
     const y = innerHeight / 2;
-    this.background = this.add.image(x, y, 'stars');
+    this.background = this.add.image(x, y, 'shiny_stars');
     this.background.displayWidth = this.sys.canvas.width;
     this.background.displayHeight = this.sys.canvas.height;
 
-    let playButton = this.add.image(x, y, 'play-white').setScale(5);
-    let creditsButton = this.add.image(x, y + 100, 'credits-white').setScale(3);
-    let formButton = this.add.image(x, y + 200, 'credits-white').setScale(4);
-    let jackieButton = this.add.image(x, y + 300, 'credits-white').setScale(4);
+    let playButton = this.add.image(x, 250, 'play-white').setScale(3);
+    let highScoreButton = this.add
+      .image(x, y + 50, 'high-score-white')
+      .setScale(1.8);
 
-    let naomiButton = this.add.text(x - 200, y, "NAOMI'S BUTTON");
+    let creditsButton = this.add
+      .image(x, y + 250, 'credits-white')
+      .setScale(5.5);
+    // let formButton = this.add.image(x, y + 200, 'credits-white').setScale(4);
+    // let jackieButton = this.add.image(x, y + 300, 'credits-white').setScale(4);
+
+    // let naomiButton = this.add.text(x - 200, y, "NAOMI'S BUTTON");
 
     playButton.setInteractive();
+    highScoreButton.setInteractive();
     creditsButton.setInteractive();
-    formButton.setInteractive();
-    jackieButton.setInteractive();
-    naomiButton.setInteractive();
+    // formButton.setInteractive();
+    // jackieButton.setInteractive();
+    // naomiButton.setInteractive();
 
     let hoverSprite1 = this.add.sprite(100, 100, 'lisa').setScale(3.5);
     hoverSprite1.setVisible(false);
@@ -49,67 +56,79 @@ class MainMenu extends Scene {
     });
 
     playButton.on('pointerover', () => {
-      playButton = this.add.image(x, y, 'play-red').setScale(5);
+      playButton = this.add.image(x, 250, 'play-red').setScale(3);
 
       hoverSprite1.setVisible(true);
       hoverSprite1.play('run');
-      hoverSprite1.x = playButton.x - 150;
-      hoverSprite1.y = playButton.y - 75;
+      hoverSprite1.x = playButton.x - 170;
+      hoverSprite1.y = playButton.y - 25;
 
       hoverSprite2.setVisible(true);
       hoverSprite2.play('alt-run');
-      hoverSprite2.x = playButton.x + 150;
-      hoverSprite2.y = playButton.y - 75;
+      hoverSprite2.x = playButton.x + 170;
+      hoverSprite2.y = playButton.y - 40;
     });
 
     playButton.on('pointerout', () => {
-      playButton = this.add.image(x, y, 'play-white').setScale(5);
+      playButton = this.add.image(x, 250, 'play-white').setScale(3);
       hoverSprite1.setVisible(false);
       hoverSprite2.setVisible(false);
     });
 
     playButton.on('pointerup', () => {
-      console.log('lesssss gooooooooooo');
+      // console.log('lesssss gooooooooooo');
       this.scene.switch('Intro');
     });
 
+    highScoreButton.on('pointerover', () => {
+      playButton = this.add.image(x, y + 50, 'high-score-red').setScale(1.8);
+    });
+
+    highScoreButton.on('pointerup', () => {
+      this.scene.switch('OneFallingScene');
+    });
+
+    highScoreButton.on('pointerout', () => {
+      playButton = this.add.image(x, y + 50, 'high-score-white').setScale(1.8);
+    });
+
     creditsButton.on('pointerover', () => {
-      creditsButton = this.add.image(x, y + 100, 'credits-red').setScale(3);
+      creditsButton = this.add.image(x, y + 250, 'credits-red').setScale(5.5);
     });
 
-    creditsButton.on('pointerout', () => {
-      creditsButton = this.add.image(x, y + 100, 'credits-white').setScale(3);
-    });
-    creditsButton.on('pointerup', () => {
-      this.scene.switch('Form');
-    });
+    // creditsButton.on('pointerout', () => {
+    //   creditsButton = this.add.image(x, y + 100, 'credits-white').setScale(3);
+    // });
+    // creditsButton.on('pointerup', () => {
+    //   this.scene.switch('Form');
+    // });
 
-    formButton.on('pointerover', () => {
-      formButton = this.add.image(x, y + 200, 'credits-red').setScale(4);
-    });
-    formButton.on('pointerout', () => {
-      formButton = this.add.image(x, y + 200, 'credits-white').setScale(4);
-    });
-    formButton.on('pointerup', () => {
-      this.scene.switch('FirstFight_Start');
-    });
+    // formButton.on('pointerover', () => {
+    //   formButton = this.add.image(x, y + 200, 'credits-red').setScale(4);
+    // });
+    // formButton.on('pointerout', () => {
+    //   formButton = this.add.image(x, y + 200, 'credits-white').setScale(4);
+    // });
+    // formButton.on('pointerup', () => {
+    //   this.scene.switch('FirstFight_Start');
+    // });
 
-    jackieButton.on('pointerover', () => {
-      jackieButton = this.add.image(x, y + 300, 'credits-red').setScale(4);
-    });
-    jackieButton.on('pointerout', () => {
-      jackieButton = this.add.image(x, y + 300, 'credits-white').setScale(4);
-    });
-    jackieButton.on('pointerup', () => {
-      this.scene.switch('Jackie');
-    });
+    // jackieButton.on('pointerover', () => {
+    //   jackieButton = this.add.image(x, y + 300, 'credits-red').setScale(4);
+    // });
+    // jackieButton.on('pointerout', () => {
+    //   jackieButton = this.add.image(x, y + 300, 'credits-white').setScale(4);
+    // });
+    // jackieButton.on('pointerup', () => {
+    //   this.scene.switch('Jackie');
+    // });
 
-    naomiButton.on('pointerout', () => {
-      console.log('clicked on Naomi button');
-    });
-    naomiButton.on('pointerup', () => {
-      this.scene.switch('PromisedLandFirst');
-    });
+    // naomiButton.on('pointerout', () => {
+    //   console.log('clicked on Naomi button');
+    // });
+    // naomiButton.on('pointerup', () => {
+    //   this.scene.switch('PromisedLandFirst');
+    // });
   }
 }
 
