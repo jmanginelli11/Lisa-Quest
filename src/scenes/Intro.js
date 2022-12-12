01a0bc1f78943143cdcbbf65cb3f0b9a464df6c5
Jackie Manginelli <jmanginelli11@gmail.com>
2022-12-12 11:23:18 -0500

    stuff

--- src/scenes/Intro.js
+++ src/scenes/Intro.js
@@ -13,25 +13,27 @@ class Intro extends Scene {
     this.background.displayWidth = this.sys.canvas.width;
     this.background.displayHeight = this.sys.canvas.height;
 
-    this.story = this.add.text(x - 250, y - 200, '').setScale(1.25);
+    this.story = this.add.text(x - x / 2, y - y / 1.2, '').setScale(x * 0.0025);
 
     this.typewriteText(
       "                \nEarth is dying. \n                \nEvil capitalists have selfishly mined our \nworld to the edge of destruction.\n                \nA small, peaceful contingent of humans \nhave put their hopes in you, Lisa.\n                \nYour mission is to find a habitable \nnew planet where humans can live \npeacefully, in balace with nature.\n                \nThis planet looks promising!\n                \nBut wait, what's that?\n                \nOh no! The capitalists are here \nto destroy this planet too!\n                \nWe can't have that! "
     );
 
     let gameButton = this.add
-      .image(x + 200, y + 300, 'letsGo-white')
-      .setScale(1.8);
+      .image(x + 200, y * 1.85, 'letsGo-white')
+      .setScale(x * 0.002);
 
     gameButton.setInteractive();
 
     gameButton.on('pointerover', () => {
-      gameButton = this.add.image(x + 200, y + 300, 'letsGo-red').setScale(1.8);
+      gameButton = this.add
+        .image(x + 200, y * 1.85, 'letsGo-red')
+        .setScale(x * 0.002);
     });
     gameButton.on('pointerout', () => {
       gameButton = this.add
-        .image(x + 200, y + 300, 'letsGo-white')
-        .setScale(1.8);
+        .image(x + 200, y * 1.85, 'letsGo-white')
+        .setScale(x * 0.002);
     });
 
     gameButton.on('pointerup', () => {
