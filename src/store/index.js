c28d9517d56e609a6ee33d3969dbb2aa352cc0ad
Sheyla De los Santos <delossantosshey@gmail.com>
2022-12-16 15:56:25 -0500

    store adjustment

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
 import axios from 'axios';
 import thunkMiddleware from 'redux-thunk';
 import scoresReducer from './redux/scoresReducer';
 
 let middleware = [thunkMiddleware.withExtraArgument({ axios })];
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger());
-}
 
 const store = createStore(scoresReducer, applyMiddleware(...middleware));
 
